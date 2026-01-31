7a0c8916b11f398aeae8ed2a674e778b515489c1
Parsa Ahmadi Nasab Emran <parsatempleowl@brandeis.edu>
2026-01-31 14:50:28 -0500

    fixed the base url for the backend express server

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -1,7 +1,7 @@
 const API_URL = "http://localhost:5000";
 
 export const fetchProducts = async () => {
-    const res = await fetch(`${API_URL}/products`);
+    const res = await fetch(`${API_URL}/api/products`);
     if (!res.ok) {
         throw new Error("Failed to fetch products");
     }
